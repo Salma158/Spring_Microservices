77431d959712d2ace51c9b5da4776713f742a72d
Salma158 <salmasherif856@gmail.com>
2024-07-18 03:03:12 +0300

    Added menu_item table schema

---
+++ Menu/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS menu_item (
+    id              INT PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    description     TEXT,
+    imageUrl        VARCHAR(255),
+    categoryId      INT NOT NULL,
+    subcategoryId   INT,
+    number_of_reviews INT DEFAULT 0,
+    average_rating   DECIMAL(2, 1) DEFAULT 0.0,
+    created_at      DATE NOT NULL,
+    created_by      VARCHAR(20) NOT NULL,
+    updated_at      DATE DEFAULT NULL,
+    updated_by      VARCHAR(20) DEFAULT NULL
+);
