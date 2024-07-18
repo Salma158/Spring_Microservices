1fad1f4c42ac9838dfc82499913a6076ea6dd8be
Salma158 <salmasherif856@gmail.com>
2024-07-18 16:02:01 +0300

    documentation enhancement

--- Menu/src/main/java/com/italianrestaurant/Menu/MenuApplication.java
+++ Menu/src/main/java/com/italianrestaurant/Menu/MenuApplication.java
@@ -1,11 +1,26 @@
 package com.italianrestaurant.Menu;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef="auditAwareImpl")
+@OpenAPIDefinition(
+		info= @Info(
+				title = "Menu microservice REST API microservice",
+				description = "Italian restaurant menu microservice REST API microservice",
+				version = "v1",
+				contact = @Contact(
+						name= "salma sherif",
+						email = "salmasherif856@gmail.com"
+				)
+
+		)
+)
 public class MenuApplication {
 
 	public static void main(String[] args) {
