87673a14fda9507e7f2bf6569f63a6dc298422a4
Salma158 <salmasherif856@gmail.com>
2024-07-18 03:27:02 +0300

    Added menu item entity

---
+++ Menu/src/main/java/com/italianrestaurant/Menu/entity/MenuItem.java
@@ -0,0 +1,22 @@
+package com.italianrestaurant.Menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class MenuItem {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Long categoryId;
+    private Long subcategoryId;
+    private Long numberOfReviews;
+    private Double averageRating;
+
+}
